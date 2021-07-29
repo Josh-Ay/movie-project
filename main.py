5e83a91a4109e1b3cce79f58fa9a337a604e5bbf
Josh-Ay <ayo.oloyede16@gmail.com>
2021-07-29 18:51:31 +0100

    Upgrade to postgre

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-from os import urandom, getenv
+from os import urandom, getenv, environ
 import requests
 from dotenv import load_dotenv
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = urandom(32)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies-collection.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get("DATABASE_URL", "sqlite:///movies-collection.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 Bootstrap(app)
