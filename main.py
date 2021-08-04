62d1dc9621786bfabedf0511b0580c8976ceda7a
Josh-Ay <ayo.oloyede16@gmail.com>
2021-08-04 14:03:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -38,6 +38,9 @@ class Movie(db.Model):
         return '<Movie %r>' % self.title
 
 
+db.create_all()
+
+
 class RateMovieForm(FlaskForm):
     rating = StringField("Your Rating Out of 10 e.g 7.5", validators=[DataRequired()])
     review = StringField("Your Review", validators=[DataRequired()])
@@ -135,5 +138,5 @@ def delete():
 
 
 if __name__ == '__main__':
-    db.create_all()
+    # db.create_all()
     app.run()
